a1be99cc5307c73ac518aac07923d3adb7fc3668
Luis Parrado <luisprmat@gmail.com>
2024-03-13 22:37:39 -0500

    Dispatch broadcast event MessageCreated from Model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,12 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
+    use BroadcastsEvents;
+
     protected $fillable = [
         'user_id',
         'message',
@@ -31,5 +36,23 @@ class Message extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class)->withDefault();
+    }
+
+    /**
+     * Get the channels that model events should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel|\Illuminate\Database\Eloquent\Model>
+     */
+    public function broadcastOn(string $event): array
+    {
+        if ($this->user_id) {
+            return [
+                new PrivateChannel("App.Models.User.{$this->user_id}"),
+            ];
+        }
+
+        return [
+            new Channel('public.newMessage'),
+        ];
     }
 }
